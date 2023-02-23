7482c6e3e3d4de92e08bcbba95ca8afe1b51db65
Jenny Boucher <jebouche@c2r4p4.hive.fi>
2023-02-23 18:14:23 +0200

    add closing of duped pipes, remove comments and debug statements

--- children.c
+++ children.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 12:49:45 by jebouche          #+#    #+#             */
-/*   Updated: 2023/02/22 17:27:07 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/02/23 14:10:34 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,96 +19,21 @@ void	pipe_child(t_command_data *cmd, char **envp)
 {
 	int		ret;
 
-
 	if (cmd->read_from < 0)
 		write(STDIN_FILENO, "\0", 1);
 	else
+	{
 		dup2(cmd->read_from, STDIN_FILENO);
+		close(cmd->read_from);
+	}
 	dup2(cmd->write_to, STDOUT_FILENO);
+	close(cmd->write_to);
 	close(cmd->to_close);
 	if (cmd->path == NULL)
-	{
-		ft_putendl_fd("child exit", 2);//
 		exit_child("command not found: ", cmd->cmd[0], 3);
-	}
+	else if (cmd->read_from < 0)
+		exit(1);
 	ret = execve(cmd->path, cmd->cmd, envp);
 	if (ret == -1)
 		exit_child("exeve failed: ", cmd->cmd[0], errno);
 }
-
-//printing for errors is not occuring...
-void	exit_child(char *error_msg, char *arg, int exit_code)
-{
-	ft_putendl_fd("child exit", 2);//
-	ft_putstr_fd("pipex: ", 2);
-	ft_putstr_fd(error_msg, 2);
-	ft_putendl_fd(arg, 2);
-	// if (exit_code)
-	exit(exit_code);
-}
-
-//make function that copies infile to outfile...
-
-// void	baby(t_pipex *pipex)
-// {
-// 	char	*temp;
-// 	char	path[1000];
-// 	int		ret;
-
-// 	dup2(pipex->p[0], 0);
-// 	dup2(pipex->outfile_fd, 1);
-// 	close(pipex->p[1]);
-// 	temp = find_correct_path(pipex->cmd2[0], pipex->paths);
-// 	if (!temp)
-// 		exit_child("command not found: ", pipex->cmd2[0], 3);
-// 	ft_strlcpy(path, temp, 1000);
-// 	free (temp);
-// 	ret = execve(path, pipex->cmd2, pipex->envp);
-// 	if (ret == -1)
-// 		exit_child("exeve failed: ", pipex->cmd2[0], errno);
-// }
-
-		// ft_putstr_fd(strerror(22), 2);
-		// ft_putstr_fd("command not found: ", 2);
-		// ft_putendl_fd(pipex->cmd1[0], 2);
-
-
-// void	firstborn(t_pipex *pipex)
-// {
-// 	char	*temp;
-// 	char	path[1000];
-// 	int		ret;
-
-// 	dup2(pipex->infile_fd, 0);
-// 	dup2(pipex->p[1], 1);
-// 	close(pipex->p[0]);
-// 	temp = find_correct_path(pipex->cmd1[0], pipex->paths);
-// 	if (!temp)
-// 	{
-// 		exit_child("command not found: ", pipex->cmd1[0], 3);
-// 	}	
-// 	ft_strlcpy(path, temp, 1000);
-// 	free (temp);
-// 	ret = execve(path, pipex->cmd1, pipex->envp);
-// 	if (ret == -1)
-// 		exit_child("exeve failed: ", pipex->cmd1[0], errno);
-// }
-
-// void	baby(t_pipex *pipex)
-// {
-// 	char	*temp;
-// 	char	path[1000];
-// 	int		ret;
-
-// 	dup2(pipex->p[0], 0);
-// 	dup2(pipex->outfile_fd, 1);
-// 	close(pipex->p[1]);
-// 	temp = find_correct_path(pipex->cmd2[0], pipex->paths);
-// 	if (!temp)
-// 		exit_child("command not found: ", pipex->cmd2[0], 3);
-// 	ft_strlcpy(path, temp, 1000);
-// 	free (temp);
-// 	ret = execve(path, pipex->cmd2, pipex->envp);
-// 	if (ret == -1)
-// 		exit_child("exeve failed: ", pipex->cmd2[0], errno);
-// }
